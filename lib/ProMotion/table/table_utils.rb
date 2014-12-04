57810106995b5b8ac9814f634f89fa1752c5e219
Jerrod Blavos <jerrod@reccenter.com>
2014-12-04 17:04:57 -0500

    tracing Issue with index_path
    erminating app due to uncaught exception 'NoMethodError', reason: 'table_utils.rb:6:in `index_path_to_section_index:': undefined method `section' for 0:Fixnum (NoMethodError)

--- lib/ProMotion/table/table_utils.rb
+++ lib/ProMotion/table/table_utils.rb
@@ -2,6 +2,7 @@ module ProMotion
   module Table
     module Utils
       def index_path_to_section_index(params)
+        puts params.to_yaml
         if params.is_a?(Hash) && params[:index_path]
           params[:section] = params[:index_path].section
           params[:index] = params[:index_path].row
